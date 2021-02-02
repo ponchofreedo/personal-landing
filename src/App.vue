81f19047d94faec5ca071e0ba4ee43ef9bc219fa
Zach Freed <zach.freed@gmail.com>
2021-02-01 22:45:10 -0500

    almost there...

--- src/App.vue
+++ src/App.vue
@@ -22,20 +22,55 @@ export default {
 }
 </script>
 
+<style>@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');</style>
+
 <style lang="scss">
-  //Global variables
-  $base__font-family: "Inter", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  // global variables
   $base__colors: (
     "steel": #A0B1BA,
     "psychic": #E065FF,
     "electric": #FFCD1C,
-    "poison": #0AE394,
-    "fire": #F47112,
+    "poison": #16D48E,
+    "fire": #E26D18,
     "ice": #2CE0D7,
     "dark": #1B2F3A,
     "ghost": #FAFAFF,
     "water": #1499FF
   );
+
+  // global mixins
+
+  // thanks Karl Merkli (https://css-tricks.com/snippets/css/less-mixin-for-rem-font-sizing/)
+  @function strip-unit($num) {
+    @return $num / ($num * 0 + 1);
+  }
+
+  @mixin rem-fallback($property, $values...) {
+    $max: length($values);
+    $pxValues: '';
+    $remValues: '';
+
+    @for $i from 1 through $max {
+      $value: strip-unit(nth($values, $i));
+      $pxValues: #{$pxValues + $value*16}px;
+
+      @if $i < $max {
+        $pxValues: #{$pxValues + " "};
+      }
+    }
+
+    @for $i from 1 through $max {
+      $value: strip-unit(nth($values, $i));
+      $remValues: #{$remValues + $value}rem;
+
+      @if $i < $max {
+        $remValues: #{$remValues + " "};
+      }
+    }
+
+    #{$property}: $pxValues;
+    #{$property}: $remValues;
+  }
 
   // @function color($key: 'dark') {
   //   @return map-get($base__colors, $key);
@@ -43,32 +78,40 @@ export default {
 
   // set up the round-robin vars and classes
   :root {
+    --base__multiplier: 8px;
     --base__font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
+    --base__font-weight: var(--font-weight--normal);
+    --base__font-size: calc(var(--base__multiplier)*2);
 
     --base__color--steel: #A0B1BA;
-    --base__color--psychic: #E065FF;
+    --base__color--psychic: #D775F0;
     --base__color--electric: #FFCD1C;
-    --base__color--poison: #0AE394;
-    --base__color--fire: #F47112;
+    --base__color--poison: #16D48E;
+    --base__color--fire: #E26D18;
     --base__color--ice: #2CE0D7;
     --base__color--dark: #1B2F3A;
     --base__color--ghost: #FAFAFF;
     --base__color--water: #1499FF;
 
-    --base__spacing-multiplier: 8px;
-    --base__spacing-1: calc(var(--base__spacing-multiplier)*1);
-    --base__spacing-2: calc(var(--base__spacing-multiplier)*2);
+    --base__spacing-1: calc(var(--base__multiplier)*1);
+    --base__spacing-2: calc(var(--base__multiplier)*2);
 
     --primary__color: var(--base__color--poison);
     --primary__background: var(--base__color--dark);
     --primary__text-color: var(--base__color--ghost);
+
+    --font-weight--normal: 400;
+    --font-weight--medium: 500;
+    --font-weight--semi: 600;
+    --font-weight--bold: 700;
   }
 
   body {
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     font-family: var(--base__font-family);
-    text-align: center;
+    font-weight: var(--base__font-weight);
+    @include rem-fallback(font-size, 1);    // equivalent to 1rem / 16px
     margin: 0;
     padding: 0;
 
@@ -111,5 +154,12 @@ export default {
 
   .conatiner {
     display: grid;
+  }
+
+  .circle-arrow__up {
+    transform: rotate(180deg);
+  }
+
+  .circle-arrow__down {
   }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,32 +1,96 @@
 <template>
   <section class="container container--octo">
+    <span class="employment-status">Open to new opportunities!</span>
+    <Donut />
     <ul>
       <li>Product designerd.</li>
       <li>Maker of tasty things.</li>
       <li>Mediocre funny man.</li>
     </ul>
+    <CircleArrow arrowClass="circle-arrow__down" svgFill="var(--primary__background)" />
   </section>
 </template>
 
 <script>
   export default {
     name: 'Header',
+    components: {
+      CircleArrow,
+      Donut
+    },
     props: {}
   }
+
+  import CircleArrow from '../components/CircleArrow.vue'
+  import Donut from '../components/Donut.vue'
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss">
 
-section {
-  grid-area: header;
-  margin-top: var(--base__spacing-2);
-  background-color: var(--primary__color);
-}
-h3 {
-  margin: 40px 0 0;
-}
-a {
-  color: #42b983;
-}
+  // thanks Karl Merkli (https://css-tricks.com/snippets/css/less-mixin-for-rem-font-sizing/)
+  @function strip-unit($num) {
+    @return $num / ($num * 0 + 1);
+  }
+
+  @mixin rem-fallback($property, $values...) {
+    $max: length($values);
+    $pxValues: '';
+    $remValues: '';
+
+    @for $i from 1 through $max {
+      $value: strip-unit(nth($values, $i));
+      $pxValues: #{$pxValues + $value*16}px;
+
+      @if $i < $max {
+        $pxValues: #{$pxValues + " "};
+      }
+    }
+
+    @for $i from 1 through $max {
+      $value: strip-unit(nth($values, $i));
+      $remValues: #{$remValues + $value}rem;
+
+      @if $i < $max {
+        $remValues: #{$remValues + " "};
+      }
+    }
+
+    #{$property}: $pxValues;
+    #{$property}: $remValues;
+  }
+
+  section {
+    position: relative;
+    grid-area: header;
+    @include rem-fallback(margin-top, 1);
+    @include rem-fallback(padding, 24, 0, 16);
+    background-color: var(--primary__color);
+    color: var(--primary__background);
+  }
+
+  .employment-status {
+    box-sizing: border-box;
+    position: absolute;
+    top: 0;
+    @include rem-fallback(margin-top, 4);
+    border: 0.25rem solid var(--base__color--dark);
+    @include rem-fallback(border-radius, 1.5);
+    @include rem-fallback(padding, 0.75, 1);
+    @include rem-fallback(font-size, 1);
+    font-weight: var(--font-weight--bold);
+    @include rem-fallback(line-height, 1);
+    text-align: center;
+  }
+
+  ul {
+    @include rem-fallback(margin, 2.5, 0);
+  }
+
+  li {
+    font-weight: var(--font-weight--bold);
+    @include rem-fallback(font-size, 4);
+    @include rem-fallback(line-height, 5.6);
+    @include rem-fallback(letter-spacing, -0.12);
+  }
 </style>
