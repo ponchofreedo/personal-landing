6543ee5aeec7d378fc5653ab18d70bf9065dbbba
Zach Freed <zach.freed@gmail.com>
2021-03-11 19:56:08 -0500

    signed an offer...dont need these things

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -3,15 +3,14 @@
     <section class="container__header container__grid">
       <section class="container__content">
         <article>
-          <h1>I'm Zach &mdash; a senior product designer crafting new and thoughtful purchasing and spend visibility experiences with the team @ <a href="https://negotiatus.com" rel="noreferrer" target="_blank">Negotiatus</a> in NYC.</h1>
-          <p>I focus on crafting thoughtful and scalable systems, practical design tooling, building accessible experiences that center around real people of all colors, shapes, and sizes, and authoring complex front-end solutions.</p>
-          <p>Previously I went beyond camping @ <a href="https://tentrr.com" rel="noreferrer" target="_blank">Tentrr</a>, built best in class travel experiences @ <a href="https://priceline.com" rel="noreferrer" target="_blank">priceline</a>, and pushed pixel performance @ <a href="https://digitalsurgeons.com" rel="noreferrer" target="_blank">Digital Surgeons</a>.</p>
-          <span class="temporary-scenario">Portfolio and ramblings in progress, but here's a link to my repo of <a href="https://github.com/ponchofreedo/portfolio-projects" rel="noreferrer" target="_blank">works and case studies</a> in the interim.</span>
+          <h1>I'm Zach &mdash; a senior product designer based in CT, USA.</h1>
+          <p>I focus on crafting thoughtful and scalable design systems, practical design tooling, building accessible experiences that center around real people of all colors, shapes, and sizes, and partnering with engineers to author complex front-end solutions.</p>
+          <p>Previously I crafted thoughtful purchasing and spend visibility experiences with the team @<a href="https://negotiatus.com" rel="noreferrer" target="_blank">Negotiatus</a>, went beyond camping @<a href="https://tentrr.com" rel="noreferrer" target="_blank">Tentrr</a>, built best in class travel experiences @<a href="https://priceline.com" rel="noreferrer" target="_blank">priceline</a>, and pushed pixel performance @<a href="https://digitalsurgeons.com" rel="noreferrer" target="_blank">Digital Surgeons</a>.</p>
         </article>
         <article>
           <span class="text--h2">Talk to me, Goose.</span>
-          <p>You've come this far on these here interwebs to find me. Unfortunately the IKEA instructions for the bat signal are pretty rough &mdash; definitely missing a wood peg &mdash; and my Rebel transponder code is a bit older and just barely "checks out," so because of those issues, feel free to take a gander at my <a href="https://1drv.ms/b/s!AiXwFLoNRqBChdwGvyiQCQl6anpn9Q" rel="noreferrer" target="_blank">r&eacute;sum&eacute;</a> or just <a href="mailto:zach.freed@gmail.com?subject=sup." rel="noreferrer">drop me a line</a>.</p>
-          <p>Or, technical issues aside, you can find me at one of these below.</p>
+          <p>You've come this far on these here interwebs to find me. Unfortunately the IKEA instructions for the bat signal are pretty rough &mdash; definitely missing a wood peg &mdash; and my Rebel transponder code is a bit older and just barely "checks out," so because of those issues, feel free to just <a href="mailto:zach.freed@gmail.com?subject=sup." rel="noreferrer">drop me a line</a>.</p>
+          <p>Or, technical issues aside, you can find me at one of these spots below.</p>
           <ul>
             <li><a href="https://linkedin.com/in/zpfreed" rel="noreferrer" target="_blank">Linkedin</a></li>
             <li><a href="https://github.com/ponchofreedo" rel="noreferrer" target="_blank">Github</a></li>
