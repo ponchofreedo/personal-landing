9e31cc35a6d5a07f5b69a3f7e9f3e64514f9426e
Zach Freed <zach.freed@gmail.com>
2024-07-21 14:44:49 -0400

    setting up the css framework

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,26 @@
 <script setup lang="ts">
 defineProps<{
-  msg: string;
-}>();
+  msg: string
+}>()
 </script>
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+    <h1>{{ msg }}</h1>
+    <h2>{{ msg }}</h2>
+    <h3>{{ msg }}</h3>
+    <h4>{{ msg }}</h4>
+    <h5>{{ msg }}</h5>
+    <small>{{ msg }}</small>
     <h3>
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-      What's next?
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
     </h3>
   </div>
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
 .greetings h1,
 .greetings h3 {
   text-align: center;
