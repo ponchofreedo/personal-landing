61bde68c1d5e6bf88d14b782b7bf060de1e1c683
Zach Freed <zach.freed@gmail.com>
2024-07-27 13:39:42 -0400

    remove refs to dead components

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <main class="main__container">
     <RouterView />
   </main>
-  <!--<header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/cv">About</RouterLink>
-      </nav>
-    </div>
-  </header>-->
 </template>
 
 <style scoped>
