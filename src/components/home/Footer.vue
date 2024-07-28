f764fde50e327d4c816f4d825f96390454be56e5
Zach Freed <zach.freed@gmail.com>
2024-07-27 21:48:34 -0400

    added missing attributes to colophon links

--- src/components/home/Footer.vue
+++ src/components/home/Footer.vue
@@ -6,7 +6,7 @@
   <footer class="container__simple footer--home" v-if="isHome === 'true'">
     <p>
       <small><b>Colophon</b></small>
-      <small>This site is meant to be a playground for experimentation. Designed in <a href="https://figma.com">Figma</a>, built with <a href="https://vuejs.org/" target="_blank">Vue</a> + <a href="https://vitejs.dev/" target="_blank">Vite</a>, authored in <a href="" target="_blank">Sublime Text</a>, and shipped with <a href="https://vercel.com/" target="_blank">Vercel</a>. Type set in <a href="https://pangrampangram.com/products/neue-montreal" target="_blank">Neue Montreal</a> by Mats Desjardins from <a href="https://pangrampangram.com/" target="_blank">Pangram Pangram Foundry</a>. Built with love and several plates of J. Kenji Lopez-Alt’s <a href="https://www.youtube.com/watch?v=Bt8YgEr-9is" target="_blank">pan-seared chicken + spring veg</a> recipe.</small>
+      <small>This site is meant to be a playground for experimentation. Designed in <a href="https://figma.com" target="_blank">Figma</a>, built with <a href="https://vuejs.org/" target="_blank">Vue</a> + <a href="https://vitejs.dev/" target="_blank">Vite</a>, authored in <a href="https://www.sublimetext.com/" target="_blank">Sublime Text</a>, and shipped with <a href="https://vercel.com/" target="_blank">Vercel</a>. Type set in <a href="https://pangrampangram.com/products/neue-montreal" target="_blank">Neue Montreal</a> by Mats Desjardins from <a href="https://pangrampangram.com/" target="_blank">Pangram Pangram Foundry</a>. Built with love and several plates of J. Kenji Lopez-Alt’s <a href="https://www.youtube.com/watch?v=Bt8YgEr-9is" target="_blank">pan-seared chicken + spring veg</a> recipe.</small>
     </p>
     <p>
       <small>&copy; 2024. Fuck the 'Rona.</small>
