edc60f45b73ebccd9fd462f05f5ac9b1b1e71f8e
Zach Freed <zach.freed@gmail.com>
2025-04-12 17:25:16 -0400

    adding the easter egg

--- src/components/global/Footer.vue
+++ src/components/global/Footer.vue
@@ -1,8 +1,55 @@
 <script setup lang="ts">
     import { ref } from 'vue'
+    // const randomCatchphrase = Math.floor(Math.random() * catchphrases.value.length);
 </script>
 
 <script lang="ts">
+    // this will render a random catchphrase in the footer next to the copyright. later, come back and add the hover interaction to show a gif in the background similar to how Mattan does his. 
+    export default {
+        data() {
+            return {
+                catchphrases: ref([
+                    { who: 'Stone Cold Steve Austin', phrase: 'Gimme a Hell Yeah!' },
+                    { who: 'Stone Cold Steve Austin', phrase: 'What?' },
+                    { who: 'Stone Cold Steve Austin', phrase: 'And that\'s the bottom line...' },
+                    { who: 'Mick Foley', phrase: 'Have a nice day!' },
+                    { who: 'Cactus Jack', phrase: 'Bang Bang!' },
+                    { who: 'Dude Love', phrase: 'Oooooow have mercy!' },
+                    { who: 'Booker T', phrase: 'Can you dig it, sucka?' },
+                    { who: 'Bret Hart', phrase: 'The best there is...' },
+                    { who: 'Chris Jericho', phrase: 'You just made the list!' },
+                    { who: 'DX', phrase: 'If you\'re not down with that...' },
+                    { who: 'DX', phrase: '...we got TWO WORDS for ya!' },
+                    { who: 'Hacksaw Jim Duggan', phrase: 'HOOOOOOOOOO!' },
+                    { who: 'Kurt Angle', phrase: 'It\'s true...it\'s damn true.' },
+                    { who: 'NWO', phrase: 'NWO for life.' },
+                    { who: 'Road Dogg', phrase: 'Oh, you did\'t know?' },
+                    { who: 'Ric Flair', phrase: 'WOOOOOOOOOOOOOO!' },
+                    { who: 'Macho Man Randy Savage', phrase: 'OHHHHH YEAAAAAAAAH!' },
+                    { who: 'The Rock', phrase: 'Know your role...' },
+                    { who: 'The Rock', phrase: 'Finally...' },
+                    { who: 'The Rock', phrase: 'If you smelllllLlLlLlLlL...' },
+                    { who: 'Ron Simmons', phrase: 'DAMN!' },
+                    { who: 'Dudley Boyz', phrase: 'GET THE TABLES!' },
+                    { who: 'Teddy Long', phrase: 'Holla, holla holla!' }
+                ]),
+                randomCatchphrase: null,
+                randomWrestler: null,
+            };
+        },
+        mounted() {
+            this.getRandomCatchphrase();
+        },
+        methods: {
+            getRandomCatchphrase() {
+                if (this.catchphrases.length > 0) {
+                    const randomIndex = Math.floor(Math.random() * this.catchphrases.length);
+                    this.randomWrestler = this.catchphrases[randomIndex].who;
+                    this.randomCatchphrase = this.catchphrases[randomIndex].phrase;
+                }
+            }
+        }
+    };
 </script>
 
 <template>
@@ -13,7 +60,7 @@
                 <p>This site is meant to be a playground for experimentation. Designed in <a href="https://figma.com" target="_blank">Figma</a>, built with <a href="https://vuejs.org" target="_blank">Vue</a>, authored in <a href="https://sublimetext.com" target="_blank">Sublime Text</a>, and shipped with <a href="https://vercel.com" target="_blank">Vercel</a>. Type set in <a href="https://pangrampangram.com/products/neue-montreal" target="_blank">Neue Montreal</a> by Mats Desjardins from <a href="https://pangrampangram.com/" target="_blank">Pangram Pangram Foundry</a>. Built with controlled chaos and my take on Thomas Keller's <a href="https://youtube.com/watch?v=Bt8YgEr-9is" target="_blank">roast chicken</a> recipe. Also built with <a href="https://instagram.com/obi_dog__kenobi_/" target="_blank">my dog</a> at my feet.</p>
             </section>
             <section>
-                <p>&copy; 2025 Zach Freed &mdash; Bang Bang!</p>
+                <p>&copy; 2025 Zach Freed &mdash; <span>{{ randomCatchphrase }}</span></p>
             </section>
         </article>
     </footer>
