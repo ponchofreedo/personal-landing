a00a0f3f82c408d5a23c311209ca6246bbb5d1d6
Zach Freed <zach.freed@gmail.com>
2025-10-31 01:42:52 -0400

    trying fix with adding image zoom lib to config
    e

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import VueImageZoomer from 'vue-image-zoomer'
 import svgLoader from 'vite-svg-loader'
 
 // https://vite.dev/config/
@@ -12,6 +13,7 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
+    VueImageZoomer,
     svgLoader({
       defaultImport: 'component'
     }),
