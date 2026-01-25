e9f4b9350f80e3c4c474b088c3c1b402347e82a7
Zach Freed <zach.freed@gmail.com>
2026-01-24 20:13:22 -0500

    moved scss out of assets into first-order directory. finished about process component.

--- src/components/about/AboutProcess.vue
+++ src/components/about/AboutProcess.vue
@@ -2,9 +2,114 @@
 
 <template>
   <section class="process__container">
-    <div class="tag tag--accent">
-      <span>How I work</span>
-    </div>
+    <header class="process__header">
+      <div class="process__tag tag tag--accent">
+        <span>How I work</span>
+      </div>
+      <div class="process__headlines">
+        <h2><em>Efficiency</em> whenever possible.</h2>
+        <h2><em>Compromise</em> when it makes sense.</h2>
+        <h2><em>Always advocate</em>, never discriminate.</h2>
+      </div>
+      <div class="process__intro">
+        <p>
+          My role is not to dominate or intimidate...it's to be a partner with my cross-functional
+          peers. I do this by advocating for the best possible outcomes that will deliver the most
+          possible impact for customers and businesses. Sometimes that means shooting from the hip;
+          sometimes that means being methodical and incremental; and sometimes it means compromising
+          to stay grounded.
+        </p>
+      </div>
+    </header>
+    <main class="process__cards-container">
+      <article class="process__card">
+        <header>
+          <figure>
+            <icon type="svg" name="iconDownload" />
+          </figure>
+          <span>01</span>
+        </header>
+        <h3>Discover.</h3>
+        <p>
+          When picking up any task, I start by defining the problem, collating or conducting
+          relevant research, and distilling JTBD that are aligned with business goals and user
+          needs.
+        </p>
+      </article>
+      <article class="process__card">
+        <header>
+          <figure>
+            <icon type="svg" name="iconDownload" />
+          </figure>
+          <span>02</span>
+        </header>
+        <h3>Prioritize.</h3>
+        <p>
+          I refine and prioritize discovery insights into clear user stories, craft relevant user
+          journeys, and identify danger zones and potential pain points.
+        </p>
+      </article>
+      <article class="process__card">
+        <header>
+          <figure>
+            <icon type="svg" name="iconDownload" />
+          </figure>
+          <span>03</span>
+        </header>
+        <h3>Re/Build.</h3>
+        <p>
+          Guerilla prototyping is the name of the game. Depending on task, I go from low-to-high
+          fidelity static flows and/or prototypes to validate solutions, and then refine as feedback
+          and concerns are communicated.
+        </p>
+      </article>
+      <article class="process__card">
+        <header>
+          <figure>
+            <icon type="svg" name="iconDownload" />
+          </figure>
+          <span>04</span>
+        </header>
+        <h3>Share.</h3>
+        <p>
+          Keep my partners and stakeholders up-to-date and share outcomes, decisions, and anything
+          else needed to maintain alignment and, to quote
+          <a
+            href="https://www.reddit.com/r/nfl/comments/1kreex5/highlight_jalen_hurts_still_keeping_the_main/"
+            target="_blank"
+            class="link link--inline"
+            >QB1</a
+          >, "keep the main thing the main thing."
+        </p>
+      </article>
+      <article class="process__card">
+        <header>
+          <figure>
+            <icon type="svg" name="iconDownload" />
+          </figure>
+          <span>05</span>
+        </header>
+        <h3>Analyze.</h3>
+        <p>
+          Towards the end, it's all about gathering data, tracking and measuring successes and
+          failures, and keeping those together to help assist with future iterations or related
+          projects.
+        </p>
+      </article>
+      <article class="process__card">
+        <header>
+          <figure>
+            <icon type="svg" name="iconDownload" />
+          </figure>
+          <span>06</span>
+        </header>
+        <h3>Party time.</h3>
+        <p>
+          Pretty self-explanatory. Design is never really done and there are always going to be new
+          insights to act on. Iteration is inevitable and can be a very, very fun process.
+        </p>
+      </article>
+    </main>
   </section>
 </template>
 
@@ -13,8 +118,9 @@
 
 .process {
   &__container {
+    display: flex;
+    flex-direction: column;
     @include container-max-width;
-    @include container-inner-grid;
     @include container-responsive-padding;
     place-items: flex-start;
     padding-top: convertRem(120px);
@@ -31,56 +137,175 @@
     @media (max-width: 480px) {
     }
   }
-}
 
-article {
-  display: grid;
-  grid-column: 1 / -1;
-  grid-template-rows: 1fr;
-  grid-template-columns: subgrid;
-
-  @media (max-width: 980px) {
+  &__header {
+    @include container-inner-grid;
+    place-items: center;
+    justify-self: center;
+    row-gap: convertRem(16px);
   }
 
-  @media (max-width: 480px) {
-    height: min-content;
-    padding-bottom: 0;
-  }
-}
-
-section {
-  // display: flex;
-  // flex-direction: column;
-  // gap: convertRem(32px);
-  // grid-column: span 8;
-  // order: 1;
-
-  // @media (max-width: 1280px) {
-  //   grid-column: span 5;
-  // }
-
-  // @media (max-width: 680px) {
-  //   grid-column: 1 / -1;
-  //   order: 2;
-  // }
-}
-
-aside {
-  display: flex;
-  flex-direction: column;
-  grid-column: span 4;
-  order: 2;
-  justify-content: center;
-
-  @media (max-width: 1280px) {
-    grid-column: span 3;
-    justify-content: flex-start;
-  }
-
-  @media (max-width: 680px) {
+  &__tag {
     grid-column: 1 / -1;
-    justify-content: flex-start;
-    order: 1;
+  }
+
+  &__headlines,
+  &__intro {
+    display: flex;
+    flex-direction: column;
+  }
+
+  &__headlines {
+    grid-column: 1 / -1;
+    gap: convertRem(2px);
+
+    @media (max-width: 480px) {
+      gap: convertRem(8px);
+    }
+
+    h2 {
+      @include text-style(h2, medium, display);
+      color: $primary__color--text--muted;
+      text-align: center;
+      text-wrap: balance;
+
+      &:first-of-type {
+        em {
+          @media (max-width: 480px) {
+            display: flex;
+            flex-direction: column;
+          }
+        }
+      }
+    }
+
+    em {
+      color: $primary__color--text;
+    }
+  }
+
+  &__intro {
+    grid-column: 4 / span 6;
+    justify-self: center;
+
+    @media (max-width: 1280px) {
+      grid-column: 2 / span 6;
+    }
+
+    @media (max-width: 980px) {
+      grid-column: 1 / -1;
+      padding-left: convertRem(40px);
+      padding-right: convertRem(40px);
+      justify-self: center;
+      justify-content: center;
+    }
+
+    @media (max-width: 680px) {
+      padding: 0;
+    }
+
+    p {
+      @include text-style(p, regular, body);
+      color: $primary__color--text--muted;
+    }
+  }
+
+  &__cards-container {
+    @include container-inner-grid;
+    place-items: center;
+    justify-self: center;
+    row-gap: convertRem(32px);
+    margin-top: convertRem(80px);
+
+    @media (max-width: 480px) {
+      row-gap: convertRem(16px);
+      margin-top: convertRem(40px);
+    }
+  }
+
+  &__card {
+    display: flex;
+    flex-direction: column;
+    row-gap: convertRem(16px);
+    grid-column: span 4;
+    border-radius: convertRem(16px);
+    border-color: $primary__color--border;
+    border-width: convertRem(2px);
+    border-style: solid;
+    padding: convertRem(40px);
+    height: 100%;
+    min-height: convertRem(400px);
+    transition:
+      background-color 0.1s ease-in-out 0s,
+      color 0.1s ease-in-out 0s;
+
+    @media (max-width: 680px) {
+      min-height: convertRem(320px);
+    }
+
+    &:hover {
+      background-color: $primary__color--background--lighter;
+
+      svg {
+        color: $primary__color--background--lighter;
+      }
+    }
+
+    header {
+      position: relative;
+
+      figure {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 50%;
+        overflow: hidden;
+        width: convertRem(64px);
+        height: convertRem(64px);
+        background-color: $primary__color--border;
+
+        @media (max-width: 680px) {
+          width: convertRem(48px);
+          height: convertRem(48px);
+        }
+      }
+
+      svg {
+        @include svgProps(
+          $height: convertRem(32px),
+          $width: convertRem(32px),
+          $scale: 1,
+          $stroke: 3,
+          $color: $primary__color--background
+        );
+
+        @media (max-width: 680px) {
+          height: convertRem(24px);
+          width: convertRem(24px);
+        }
+      }
+
+      span {
+        position: absolute;
+        top: 0;
+        right: 0;
+        text-align: right;
+        @include text-style(big, medium, fixed);
+        color: $primary__color--text--darker;
+
+        @media (max-width: 680px) {
+          font-size: convertRem(18px);
+        }
+      }
+    }
+
+    h3 {
+      color: $primary__color--accent;
+    }
+
+    p {
+      color: $primary__color--text--muted;
+    }
   }
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
       '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
-      '@css': fileURLToPath(new URL('./src/assets/css', import.meta.url)),
+      '@css': fileURLToPath(new URL('./src/css', import.meta.url)),
       '@data': fileURLToPath(new URL('./src/data', import.meta.url)),
       '@fonts': fileURLToPath(new URL('./src/assets/fonts', import.meta.url)),
       '@global': fileURLToPath(new URL('./src/components/global', import.meta.url)),
@@ -35,7 +35,7 @@ export default defineConfig({
       '@interfaces': fileURLToPath(new URL('./src/interfaces', import.meta.url)),
       '@layout': fileURLToPath(new URL('./src/components/layout', import.meta.url)),
       '@pdf': fileURLToPath(new URL('./src/assets/pdf', import.meta.url)),
-      '@scss': fileURLToPath(new URL('./src/assets/scss', import.meta.url)),
+      '@scss': fileURLToPath(new URL('./src/scss', import.meta.url)),
       '@svg': fileURLToPath(new URL('./src/components/svg', import.meta.url)),
       '@util': fileURLToPath(new URL('./src/components/util', import.meta.url))
     }
